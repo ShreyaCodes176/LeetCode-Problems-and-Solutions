0001a0341bf5d2e77e9ba7a824e364bac9473931
Shreya Ghoshal <shreya.ghoshal176@gmail.com>
2026-07-15 17:09:17 +0530

    Time: 444 ms (73.8%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,10 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    SET N=N-1;
+    SET N = N - 1;
+
     RETURN (
-        SELECT DISTINCT salary 
-        FROM Employee 
-        ORDER BY salary DESC 
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
         LIMIT 1 OFFSET N
-  );
+    );
 END
