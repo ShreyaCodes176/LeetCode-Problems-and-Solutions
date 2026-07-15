82755ccc082db71f7110b8a7d3b7659e2fe8b06c
Shreya Ghoshal <shreya.ghoshal176@gmail.com>
2026-07-15 16:35:19 +0530

    Time: 3 ms (43.08%), Space: 19.3 MB (21.29%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dcd(a,b):
+            if b==0:
+                return a
+            else:
+                gcd(b,a%b)
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        sumOdd=n*n
+        sumEven=n*(n+1)
+        return gcd(sumOdd,sumEven)
