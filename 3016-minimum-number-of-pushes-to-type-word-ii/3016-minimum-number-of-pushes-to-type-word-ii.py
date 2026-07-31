081af8103b441d880de70b65017ae59da8e3ed6c
Shreya Ghoshal <shreya.ghoshal176@gmail.com>
2026-07-31 16:01:20 +0530

    Time: 181 ms (33.78%), Space: 20.1 MB (48.16%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq=[0]*26
+        for c in word:
+            freq[ord(c)-ord('a')]+=1
+        freq.sort(reverse=True)
+        total=0
+        for i in range(26):
+            if(freq[i]==0):
+                break
+            total+=(i//8+1)*freq[i]
+        return total
