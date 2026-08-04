8707debc2163a2ce4356eeb7c986ba4d2679f494
Shreya Ghoshal <shreya.ghoshal176@gmail.com>
2026-08-04 18:29:19 +0530

    Time: 4 ms (41.53%), Space: 19.2 MB (55.74%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -3,7 +3,7 @@ class Solution:
         res=[]
         minv=min(nums)
         maxv=max(nums)
-        for i in range(minv,maxv,1):
+        for i in range(minv+1,maxv):
             if i not in nums:
                 res.append(i)
         return res
